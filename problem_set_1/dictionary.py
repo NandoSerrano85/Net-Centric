4e4a11cf5f2fd34341b97a9b05216a81ef23b204
NandoSerrano85 <fserr009@fiu.edu>
2017-08-22 23:46:36 -0400

    initial steps to creating dictionary to help quiz user on definition words for net-centric computing

--- problem_set_1/dictionary.py
+++ problem_set_1/dictionary.py
@@ -1,4 +1,18 @@
 #code to help me with definition blocks from problem set 1
 import json, re
+from random import randint
 
+def main():
+    doc = open('dict.json', 'r')
+    dictionary = json.loads(doc.read())
+    end = False
+    while end == False:
+        # n = randint(0,len(dictionary)-1)
+        print "inside loop\n"
+        key, value = dictionary['host']
+        print key + "\n" + value
+        # word = input("what is the definition: ")
+        end = True
+    search = None
 
+main()
